81cf4bc2879e7e08d65d59dc143a333758707eb7
Myst33d <notmyst33d@gmail.com>
2024-10-13 23:42:34 +0600

    Use lightweight ureq instead of reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use regex::Regex;
 use std::env;
 use std::fs::write;
 use std::io::Read;
 use std::path::Path;
-use std::collections::HashMap;
-use reqwest::blocking::Client;
-use regex::Regex;
+use ureq::AgentBuilder;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -16,40 +15,40 @@ fn main() {
         }
     };
 
-    let mut data: Vec<u8> = vec![];
-    let mut params: HashMap<&str, &str> = HashMap::new();
-    let client = Client::new();
+    let client = AgentBuilder::new().build();
+    let response = client
+        .post("http://imtranslator.net/translate-and-speak/sockets/tts.asp?FA=0&dir=es&speed=0&B=1&ID=&chr=Jason&vc=Jorge")
+        .set("Referer", "e") // Dont touch or shit will break
+        .send_form(&[
+            ("text", text),
+            ("chr", "Jason"),
+            ("speed", "0"),
+            ("voice", ""),
+            ("dir", "es"),
+            ("B", "1"),
+        ]).unwrap()
+        .into_string().unwrap();
 
-    params.insert("text", text);
-    params.insert("chr", "Jason");
-    params.insert("speed", "0");
-    params.insert("voice", "");
-    params.insert("dir", "es");
-    params.insert("B", "1");
-
-    let mut response = client
-        .post("https://imtranslator.net/translate-and-speak/sockets/tts.asp?FA=0&dir=es&speed=0&B=1&ID=&chr=Jason&vc=Jorge")
-        .form(&params)
-        .header("Referer", "e") // Dont touch or shit will break
-        .send()
-        .unwrap();
-
-    response.read_to_end(&mut data).unwrap();
-    let string_data = String::from_utf8(data.clone()).unwrap();
-
-    let re = Regex::new(r"(loader-wav\.asp\?)(.*)(cs_[0-9_]*_)(.*)(ID=)([0-9]*)(.*)(servercode=)([0-9]*)").unwrap();
-    let caps = re.captures(&string_data).unwrap();
+    let re = Regex::new(
+        r"(loader-wav\.asp\?)(.*)(cs_[0-9_]*_)(.*)(ID=)([0-9]*)(.*)(servercode=)([0-9]*)",
+    )
+    .unwrap();
+    let caps = re.captures(&response).unwrap();
     let name = &caps[3];
     let id = &caps[6];
     let param = &caps[9];
 
-    let mut wav_response = client
-        .get(format!("https://cs1.imtranslator.net/SL/Free_Projects/{}/{}1.wav?param1={}", id, name, param))
-        .send()
+    let mut bytes = vec![];
+    client
+        .get(&format!(
+            "http://cs1.imtranslator.net/SL/Free_Projects/{}/{}1.wav?param1={}",
+            id, name, param
+        ))
+        .call()
+        .unwrap()
+        .into_reader()
+        .read_to_end(&mut bytes)
         .unwrap();
-
-    data.clear();
-    wav_response.read_to_end(&mut data).unwrap();
 
     let mut collision_counter = 0;
     let mut path_string = format!("audio{}.wav", collision_counter);
@@ -60,7 +59,7 @@ fn main() {
         path = Path::new(&path_string);
     }
 
-    write(path, data).unwrap();
+    write(path, bytes).unwrap();
 
     println!("Audio written to {}", path.display());
 }
